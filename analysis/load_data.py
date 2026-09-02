3be064fe33a8cf8392d51c62ace89ba94b76ae73
jtc469 <jamie@clines.co.uk>
2026-09-02 12:06:27 -0500

    Seperated loader & plotter

--- analysis/load_data.py
+++ analysis/load_data.py
@@ -1,79 +1,31 @@
 from pathlib import Path
-import argparse
 import pandas as pd
-import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(prog="NCCL-Tests Data Loader")
+def load_csvs(system, job_id):
 
-parser.add_argument("system", help="System name, e.g. aac6 or bluebear")
-parser.add_argument("job_id", help="Slurm job ID")
+    benchmark_names = [
+        "all_gather_perf",
+        "all_reduce_perf",
+        "broadcast_perf",
+        "reduce_scatter_perf",
+    ]
 
-args = parser.parse_args()
+    FILE_HEAD =  Path("out/raw") / f"{system}-{job_id}"
 
-system = args.system
-job_id = args.job_id
+    benchmarks = {
+        name: pd.read_csv(f"{FILE_HEAD}/{system}-{name}-{job_id}.csv")
+        for name in benchmark_names
+    }
 
-benchmark_names = [
-    "all_gather_perf",
-    "all_reduce_perf",
-    "broadcast_perf",
-    "reduce_scatter_perf",
-]
+    metadata = pd.read_csv(
+        f"{FILE_HEAD}/metadata-{job_id}.csv"
+    ).iloc[0].to_dict()
 
-FILE_HEAD =  Path("out/raw") / f"{system}-{job_id}"
+    for name, df in benchmarks.items():
+        benchmarks[name] = df.assign(**metadata)
 
-benchmarks = {
-    name: pd.read_csv(f"{FILE_HEAD}/{system}-{name}-{job_id}.csv")
-    for name in benchmark_names
-}
+    partition = metadata["partition"]
+    ngpus = metadata["ngpus"]
 
-metadata = pd.read_csv(
-    f"{FILE_HEAD}/metadata-{job_id}.csv"
-).iloc[0].to_dict()
+    return benchmarks, partition, ngpus
 
-for name, df in benchmarks.items():
-    benchmarks[name] = df.assign(**metadata)
-
-partition = metadata["partition"]
-ngpus = metadata["ngpus"]
-
-for b in benchmarks:
-
-    name = b.replace("perf", "")
-
-    Path(f"out/plots/{system}-{job_id}").mkdir(parents=True, exist_ok=True)
-
-    df = benchmarks[b]
-
-    ax = df.plot(
-        x="size",
-        y="time",
-        marker="o",
-        legend=False
-    )
-
-    ax.set_xscale("log", base=2)
-    ax.set_xlabel("Message size (bytes)")
-    ax.set_ylabel("Time (µs)")
-    ax.set_title(f"{system} {name} latency")
-    ax.grid(True, alpha=0.3)
-
-    plt.tight_layout()
-
-    # GPU metadata note
-    ax.text(
-    0.02,
-    0.98,
-    f"Partition: {partition}\nGPUs: {ngpus}",
-    transform=ax.transAxes,
-    verticalalignment="top",
-    fontsize=8,
-    bbox={
-        "boxstyle": "round",
-        "facecolor": "white",
-        "alpha": 0.8,
-    },
-    )
-
-    plt.savefig(f"out/plots/{system}-{job_id}/{system}-{name}.png", dpi=200)
-    plt.close()
---
+++ analysis/plot_charts.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from load_data import load_csvs
+
+parser = argparse.ArgumentParser(prog="NCCL-Tests Data Loader")
+
+parser.add_argument("system", help="System name, e.g. aac6 or bluebear")
+parser.add_argument("job_id", help="Slurm job ID")
+
+args = parser.parse_args()
+
+system = args.system
+job_id = args.job_id
+
+
+benchmarks, partition, ngpus = load_csvs(system, job_id)
+
+for b in benchmarks:
+
+    name = b.replace("perf", "")
+
+    Path(f"out/plots/{system}-{job_id}").mkdir(parents=True, exist_ok=True)
+
+    df = benchmarks[b]
+
+    ax = df.plot(
+        x="size",
+        y="time",
+        marker="o",
+        legend=False
+    )
+
+    ax.set_xscale("log", base=2)
+    ax.set_xlabel("Message size (bytes)")
+    ax.set_ylabel("Time (µs)")
+    ax.set_title(f"{system} {name} latency")
+    ax.grid(True, alpha=0.3)
+
+    plt.tight_layout()
+
+    # GPU metadata note
+    ax.text(
+    0.02,
+    0.98,
+    f"Partition: {partition}\nGPUs: {ngpus}",
+    transform=ax.transAxes,
+    verticalalignment="top",
+    fontsize=8,
+    bbox={
+        "boxstyle": "round",
+        "facecolor": "white",
+        "alpha": 0.8,
+    },
+    )
+
+    plt.savefig(f"out/plots/{system}-{job_id}/{system}-{name}.png", dpi=200)
+    plt.close()
